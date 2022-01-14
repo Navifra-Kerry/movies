eb8e97a49b59ad41ce3300b1fe73a094079b9695
Kerry-Cho <cdh0012@naver.com>
2022-01-14 17:42:51 +0900

    add docker

--- server.ts
+++ server.ts
@@ -1,57 +1,106 @@
-import express, { Request, Response, NextFunction } from 'express';
-import cors from 'cors';
-import morgan from 'morgan';
-import {
-  createProxyMiddleware,
-  Filter,
-  Options,
-  RequestHandler,
-} from 'http-proxy-middleware';
+import express from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 import { createTerminus } from '@godaddy/terminus';
+import morgan from 'morgan';
+import axios from 'axios';
+
+import { createLogger, format, transports } from 'winston';
+import DailyRotateFile from 'winston-daily-rotate-file';
+
+const env = process.env.NODE_ENV || 'development';
+const logDir = 'log';
+
+const dailyRotateFileTransport = new DailyRotateFile({
+  level: 'debug',
+  filename: `${logDir}/%DATE%-server.log`,
+  datePattern: 'YYYY-MM-DD',
+  zippedArchive: true,
+  maxSize: '20m',
+  maxFiles: '14d',
+});
+
+export const logger = createLogger({
+  exitOnError: false,
+  level: env === 'development' ? 'debug' : 'info',
+  format: format.combine(
+    format.timestamp({
+      format: 'DD/MMM/YYYY:HH:mm:ss ZD',
+    }),
+    format((info) => {
+      info.level = info.level.toUpperCase();
+      return info;
+    })(),
+    format.json(),
+  ),
+  exceptionHandlers: [
+    new transports.File({ filename: `${logDir}/exceptions.log` }),
+  ],
+  transports: [
+    new transports.Console({
+      level: 'http',
+      format: format.combine(
+        format.colorize(),
+        format.printf(
+          (info) => `[${info.timestamp}] [${info.level}] ${info.message}`,
+        ),
+      ),
+      silent: false,
+    }),
+    dailyRotateFileTransport,
+  ],
+});
+
+logger.on('error', (err) => {});
 
 function bootstrap() {
   const app = express();
-  const port = process.env.GUI_PORT ? process.env.GUI_PORT : 8000;
+  const port = process.env.PORT ? process.env.PORT : 8000;
+  const apiServerUrl = process.env.API_SERVER
+    ? process.env.API_SERVER
+    : 'http://localhost:4000';
+
   const options = {
-    target: 'http://localhost:4000', // target host
-    //changeOrigin: true, // needed for virtual hosted sites
-    // ws: true, // proxy websockets
+    target: apiServerUrl,
     pathRewrite: function (path: any, req: any) {
       return path.replace('/api', '');
     },
   };
-  app.use('/', createProxyMiddleware(options));
-  // app.use(
-  //   cors({
-  //     origin: true,
-  //     credentials: true,
-  //     optionsSuccessStatus: 200,
-  //   }),
-  // );
 
-  //const onSignal = async () => {
-  //  // logger.info('Server is starting cleanup');
-  //  // await redisSubscriber.finalize();
-  //  // io.close();
-  //  // clientManager.remove();
-  //  // redisSubscriber.remove();
-  //};
-  //
-  //const onHealthCheck = async () => {
-  //  // redis가 접속이 끊어지면 컨테이너 이미지 강제 종 료
-  //  //return redisSubscriber.onHealthCheck();
-  //};
+  app.use(
+    morgan(
+      `":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"`,
+      {
+        stream: {
+          write: (message: string) => {
+            logger.http(message);
+          },
+        },
+      },
+    ),
+  );
+
+  app.use(express.static('./build'));
+  app.use('/api', createProxyMiddleware(options));
+
+  const onHealthCheck = async () => {
+    // return axios.get(`${apiServerUrl}/health`).then(res=> return res);
+  };
+
+  const onSignal = async () => {
+    logger.info('Server is starting cleanup');
+    server.close();
+  };
 
   const server = app.listen(port);
 
-  console.log('server started');
+  createTerminus(server, {
+    signal: 'SIGINT',
+    healthChecks: {
+      '/health': onHealthCheck,
+    },
+    onSignal,
+  });
 
-  //createTerminus(server, {
-  //  signal: 'SIGINT',
-  //  healthChecks: {
-  //    '/_health/liveness': onHealthCheck,
-  //  },
-  //  onSignal,
-  //});
+  logger.info(`Server is running at ${port}`);
 }
 bootstrap();
